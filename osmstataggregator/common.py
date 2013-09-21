5b038f3cc3bef9cedb651de16a536d63cc2f3e34
Rory McCann <rory@technomancy.org>
2013-09-21 13:05:00 +0100

    split into areas file

--- osmstataggregator/common.py
+++ osmstataggregator/common.py
@@ -452,31 +452,3 @@ class OSMStatsAggregator(object):
             # Commit anything unsaved yet
             self.database_connection().commit()
         return
-
-# Some common areas
-class IrelandArea(object):
-    top = 55.9
-    bottom = 51.1
-    left = -10.9
-    right = -4.7
-    increment = 0.1
-
-class EuropeArea(object):
-    top = 71.5
-    bottom = 36.2
-    left = -25
-    right = 50.3
-    increment = 0.1
-
-class BritainAndIrelandArea(object):
-    top = 59.7
-    bottom = 49.8
-    right = 2.1
-    left = -10.9
-    increment = 0.025
-
-class NorthAmericaArea(object):
-    top = 74.12
-    bottom = 17.64
-    left = -169.45
-    right = -51.33
